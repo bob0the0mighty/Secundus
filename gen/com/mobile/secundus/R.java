46874013e283a405267be0429f307b9da4383f92
David Patterson <bob0the0mighty@gmail.com>
2013-01-27 17:49:38 -0500

    Looks stable. No obvious bugs

--- gen/com/mobile/secundus/R.java
+++ gen/com/mobile/secundus/R.java
@@ -47,24 +47,25 @@ public final class R {
         public static final int activity_alert_test=0x7f070000;
     }
     public static final class string {
-        public static final int alert=0x7f050006;
+        public static final int alert=0x7f050007;
         public static final int app_name=0x7f050000;
-        public static final int blue=0x7f05000a;
-        public static final int correct_static=0x7f050004;
-        public static final int dont_drive=0x7f05000e;
-        public static final int dont_walk=0x7f05000d;
-        public static final int green=0x7f05000b;
-        public static final int menu_settings=0x7f050007;
-        public static final int ok=0x7f050010;
-        public static final int pay_attention=0x7f05000f;
-        public static final int perfect=0x7f050011;
-        public static final int pink=0x7f05000c;
-        public static final int press=0x7f050008;
-        public static final int red=0x7f050009;
-        public static final int reset_button=0x7f050003;
+        public static final int blue=0x7f05000b;
+        public static final int correct_static=0x7f050005;
+        public static final int dont_drive=0x7f05000f;
+        public static final int dont_walk=0x7f05000e;
+        public static final int green=0x7f05000c;
+        public static final int menu_settings=0x7f050008;
+        public static final int ok=0x7f050011;
+        public static final int pay_attention=0x7f050010;
+        public static final int perfect=0x7f050012;
+        public static final int pink=0x7f05000d;
+        public static final int press=0x7f050009;
+        public static final int red=0x7f05000a;
+        public static final int reset_button=0x7f050004;
         public static final int start_button=0x7f050001;
+        public static final int start_button_finished=0x7f050003;
         public static final int start_button_started=0x7f050002;
-        public static final int wrong_static=0x7f050005;
+        public static final int wrong_static=0x7f050006;
     }
     public static final class style {
         /** 
--- src/com/mobile/secundus/AlertTestActivity.java
+++ src/com/mobile/secundus/AlertTestActivity.java
@@ -12,26 +12,32 @@ import android.widget.TextView;
 
 public class AlertTestActivity extends Activity {
 
-	private final int			NUMBER_ROUNDS					= 10;
-	private final Random	RNG										= new Random();
-	private final String	CORRECT_COLOR_STRING	= "CORRECT_COLOR_STRING";
-	private final String	GUESS_COUNT_INTARRAY	= "GUESS_COUNT_INTARRAY";
-	private Button				startButton, resetButton, topLeft, topRight,
-												bottomLeft, bottomRight;
-	private TextView			alert, correct, wrong;
-	private String				correctColor;
-	private int						correctSelections, wrongSelections;
-	private int[]					fromSavedState;
-
+	private final int				NUMBER_ROUNDS					= 10;
+	private final Double		WORST									= 0.4;
+	private final Double		WORSE									= 0.6;
+	private final Double		BAD										= 0.8;
+	private final Random		RNG										= new Random();
+	private final String		CORRECT_COLOR_STRING	= "CORRECT_COLOR_STRING";
+	private final String		GUESS_COUNT_INTARRAY	= "GUESS_COUNT_INTARRAY";
+	private final String[]	colors 								= { "red", "blue", "green", "pink" };
+	private int							correctSelections, wrongSelections;
+	private int[]						fromSavedState;
+	private Button					startButton, resetButton, topLeft, topRight,
+													bottomLeft, bottomRight;
+	private TextView				alert, correct, wrong;
+	private String					correctColor;
+	
+	@Override
 	protected void onCreate( Bundle savedInstanceState ) {
 		super.onCreate( savedInstanceState );
 		setContentView( R.layout.main );
+		//First branch if the game state was saved due to orientation change, home button press, etc...
 		if( savedInstanceState != null && savedInstanceState.containsKey( CORRECT_COLOR_STRING ) ){
 			correctColor 			= savedInstanceState.getString( CORRECT_COLOR_STRING );
 			fromSavedState 		= savedInstanceState.getIntArray( GUESS_COUNT_INTARRAY );
 			correctSelections = fromSavedState[0];
 			wrongSelections		= fromSavedState[1];
-		} else {
+		} else { //new program instance
 			correctColor 			= null;
 			correctSelections = 0;
 			wrongSelections 	= 0;
@@ -39,24 +45,43 @@ public class AlertTestActivity extends Activity {
 		RunGame();
 	}
 	
+	/*
+	 * Ensures game state propigation when program re-ups for various
+	 * reasons.  The correct color and current score are the important pieces.
+	 * (non-Javadoc)
+	 * @see android.app.Activity#onSaveInstanceState(android.os.Bundle)
+	 */
+	@Override
 	public void onSaveInstanceState( Bundle savedInstanceState ) {
 		super.onSaveInstanceState( savedInstanceState );
 		savedInstanceState.putString( CORRECT_COLOR_STRING, correctColor );
 		savedInstanceState.putIntArray( GUESS_COUNT_INTARRAY, new int[]{ correctSelections, wrongSelections }  );
 	}
 
+	/*
+	 * Shows no option menu
+	 * (non-Javadoc)
+	 * @see android.app.Activity#onCreateOptionsMenu(android.view.Menu)
+	 */
+	@Override
 	public boolean onCreateOptionsMenu( Menu menu ) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate( R.menu.activity_alert_test, menu );
-		return true;
+		//getMenuInflater().inflate( R.menu.activity_alert_test, menu );
+		return false;
 	}
 	
+	/*
+	 * Main logic entrance
+	 */
 	private void RunGame() {
 		SetButtons();
 		SetTextViews();
 		updateText();
 	}
 
+	/*
+	 * Sets pointers for text boxes and sets initial score.
+	 */
 	private void SetTextViews() {
 		alert 	= (TextView) findViewById( R.id.alert_dynamic_text );
 		correct = (TextView) findViewById( R.id.correct_dynamic_text );
@@ -65,6 +90,10 @@ public class AlertTestActivity extends Activity {
 		wrong.setText( "" + wrongSelections );
 	}
 
+	/*
+	 * Sets pointers for buttons and then assigns onClickListeners 
+	 * for each.
+	 */
 	private void SetButtons() {
 		startButton = (Button) findViewById( R.id.start_button );
 		resetButton = (Button) findViewById( R.id.reset_button );
@@ -75,11 +104,17 @@ public class AlertTestActivity extends Activity {
 		setClickListeners();
 	}
 
+	/*
+	 * I chose this method as I'm more familiar with it than others allowed by android
+	 * and, in the case of the color buttons, I couldn't find a way that I though was
+	 * better than this for passing variables into a reusable function.  The end result 
+	 * is less flexible than I like.
+	 */
 	private void setClickListeners() {
 		startButton.setOnClickListener( new OnClickListener() {
 			public void onClick( View v ) {
 				if ( correctColor == null ) {
-					reset();//just in case
+					reset();//ensure clean state
 					selectNewColor();
 					updateText();
 					turnOffStartButton();
@@ -96,37 +131,32 @@ public class AlertTestActivity extends Activity {
 
 		topLeft.setOnClickListener( new OnClickListener() {
 			public void onClick( View v ) {
-				if ( correctColor != null ) {
-					buttonSelection("red");
-				}
+				buttonSelection(colors[0]);
 			}
 		} );
 
 		topRight.setOnClickListener( new OnClickListener() {
 			public void onClick( View v ) {
-				if ( correctColor != null ) {
-					buttonSelection("blue");
-				}
+				buttonSelection(colors[1]);
 			}
 		} );
 
 		bottomLeft.setOnClickListener( new OnClickListener() {
 			public void onClick( View v ) {
-				if ( correctColor != null ) {
-					buttonSelection("green");
-				}
+				buttonSelection(colors[2]);
 			}
 		} );
 
 		bottomRight.setOnClickListener( new OnClickListener() {
 			public void onClick( View v ) {
-				if ( correctColor != null ) {
-					buttonSelection("pink");
-				}
+				buttonSelection(colors[3]);
 			}
 		} );
 	}
 
+	/*
+	 * Set game to start state
+	 */
 	private void reset() {
 		correctColor = null;
 		correctSelections = 0;
@@ -135,56 +165,78 @@ public class AlertTestActivity extends Activity {
 		startButton.setText( R.string.start_button );
 	}
 
+	/*
+	 * Manage the updating of various texts while game is in different states.
+	 * I see before start, during play, and end game as the states that need to
+	 * be handled.  The matching number of branches fills me with hope.
+	 */
 	private void updateText() {
 		correct.setText( "" + correctSelections );
 		wrong.setText( "" + wrongSelections );
 		if ( correctColor == null && ( correctSelections + wrongSelections ) == 0 ) {
 			alert.setText( "" );
 		} else if ( ( correctSelections + wrongSelections ) >= NUMBER_ROUNDS) {
-			if( startButton.isClickable() ){
-				turnOffStartButton();
-			}
+			turnOffStartButton();
 			evaluateUser();
+			startButton.setText( R.string.start_button_finished );
 			correctColor = null;
 		} else {
-			if( startButton.isClickable() ){
-				turnOffStartButton();
-			}
+		  turnOffStartButton();
 			alert.setText( getString( R.string.press ) + " " + correctColor );
 		}
 	}
 
+	/*
+	 * Select a new color from on in the string.xml file. Feels more Androidish than
+	 * using the static local color array.  Would be more usefull if I could decoupl
+	 * the clickListeners from the local array.
+	 */
 	private void selectNewColor() {
 		int index = RNG.nextInt( R.color.class.getDeclaredFields().length );
 		correctColor = R.color.class.getDeclaredFields()[index].getName();
 	}
 
+	/*
+	 * Changed from given code in order to allow for
+	 * changes to NUMBER_ROUNDS
+	 */
 	private void evaluateUser() {
-		if ( correctSelections < 4 ) {
+		if ( correctSelections < NUMBER_ROUNDS * WORST ) {
 			alert.setText( R.string.dont_walk);
-		} else if ( correctSelections < 6 ) {
+		} else if ( correctSelections < NUMBER_ROUNDS * WORSE ) {
 			alert.setText( R.string.dont_drive);
-		} else if ( correctSelections < 8 ) {
+		} else if ( correctSelections < NUMBER_ROUNDS * BAD ) {
 			alert.setText( R.string.pay_attention);
-		} else if ( correctSelections < 10 ) {
+		} else if ( correctSelections < NUMBER_ROUNDS ) {
 			alert.setText( R.string.ok);
 		} else {
 			alert.setText( R.string.perfect);
 		}
 	}
 	
+	/*
+	 * Ensures game cannot restart while in progress without using the reset button
+	 * first.
+	 */
 	private void turnOffStartButton() {
-		startButton.setClickable( false );
-		startButton.setText( R.string.start_button_started );
+		if( startButton.isClickable() ) {
+			startButton.setClickable( false );
+			startButton.setText( R.string.start_button_started );
+		}
 	}
 
+	/*
+	 * Checks if clicked button is the correct one.
+	 */
 	private void buttonSelection(String color) {
-		if ( correctColor.equals( color ) ) {
-			correctSelections++;
-		} else {
-			wrongSelections++;
+		if ( correctColor != null ) {
+			if ( correctColor.equals( color ) ) {
+				correctSelections++;
+			} else {
+				wrongSelections++;
+			}
+			selectNewColor();
+			updateText();
 		}
-		selectNewColor();
-		updateText();
 	}
 }
